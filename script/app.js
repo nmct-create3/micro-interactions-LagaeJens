2a736ac8eab3643ec446fb5160ac8898e60325e7
Jens Lagae <jens.lagae@student.howest.be>
2022-10-06 14:58:21 +0200

    commit

--- script/app.js
+++ script/app.js
@@ -1,9 +1,17 @@
 function handleFloatingLabel() {
-   
+
 }
 
 function handlePasswordSwitcher() {
-    
+    document.querySelector('.js-togglePassword').addEventListener('click', function () {
+        const passwordInput = document.querySelector('togglePassword');
+        if (passwordInput.getAttribute('type') === 'password') {
+            passwordInput.setAttribute('type', 'text');
+        } else {
+            passwordInput.setAttribute('type', 'password');
+        }
+    }
+    );
 }
 
 document.addEventListener('DOMContentLoaded', function () {
